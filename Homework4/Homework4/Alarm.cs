2e73950a933d303039e9b2d4a72fce3a2740febc
aimerya <49513203+aimerya@users.noreply.github.com>
2020-03-18 21:17:05 +0800

    Add files via upload

---
+++ Homework4/Homework4/Alarm.cs
@@ -0,0 +1,64 @@
+﻿//2、使用事件机制，模拟实现一个闹钟功能。闹钟可以有嘀嗒（Tick）事件和响铃（Alarm）两个事件。
+//在闹钟走时时或者响铃时，在控制台显示提示信息。
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Alarm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Now it is:" + DateTime.Now.ToLongTimeString().ToString());
+            Console.WriteLine("Set the alarm:(xx:xx:xx)");
+            string str = Console.ReadLine();
+            ClockEvents ce = new ClockEvents();
+            ce.ac.Clock(str);
+        }
+    }
+
+    public delegate void ClockHandler();
+
+    public class AlarmClock
+    {
+        public event ClockHandler Tick;
+        public event ClockHandler Alarm;
+
+        public void Clock(string setTime)
+        {
+            while (true)
+            {
+                string time = DateTime.Now.ToLongTimeString().ToString();
+                Tick();
+                System.Threading.Thread.Sleep(1000);
+                if (time == setTime)
+                    Alarm();
+            }
+        }
+    }
+
+    public class ClockEvents
+    {
+        public AlarmClock ac = new AlarmClock();
+
+        public ClockEvents()
+        {
+            ac.Alarm += Clock_Alarm;
+            ac.Tick += Clock_Tick;
+        }
+
+        void Clock_Tick()
+        {
+            Console.WriteLine("Tick" + DateTime.Now.ToLongTimeString().ToString());
+        }
+
+        void Clock_Alarm()
+        {
+            Console.WriteLine("Alarm");
+        }
+    }
+}
---
+++ Homework4/Homework4/ListFEach.cs
@@ -0,0 +1,71 @@
+﻿//1、为示例中的泛型链表类添加类似于List<T>类的ForEach(Action<T> action)方法。
+//通过调用这个方法打印链表元素，求最大值、最小值和求和（使用lambda表达式实现）。
+
+
+
+using System;
+using System.Collections.Generic;
+
+
+namespace Homework4
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class List<T>
+    {
+        private Node<T> head { get; set; }
+        private Node<T> tail { get; set; }
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> x = head; x != null; x = x.Next)
+            {
+                action(x.Data);
+            }
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+    }
+
+    class ListFEach
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                list.Add(x);
+            }
+            list.ForEach(x => Console.WriteLine(x));
+            int max = 0;
+            int min=0;
+            int sum = 0;
+            list.ForEach(s => { max = max < s ? s : max; });
+            list.ForEach(s => { min = min > s ? s : min; });
+            list.ForEach(delegate (int x) { sum += x; });
+            Console.WriteLine("最大值为：" + max);
+            Console.WriteLine("最小值为：" + min);
+            Console.WriteLine("和为："+sum);
+        }
+    }
+}
