74cdc2b77cc6a528a7f29240bd27d2263a373ead
aimerya <49513203+aimerya@users.noreply.github.com>
2020-05-26 16:47:42 +0800

    Homework9

---
+++ Homework9/SimpleCrawler/SimpleCrawlerForm/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("SimpleCrawlerForm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleCrawlerForm")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8245bf40-b637-4c45-8589-8e5c64e0e8e1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
